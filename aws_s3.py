15b641c88b077780e3dfa572aeb79c0a40f8baf7
Gowtham Nayak <imperial.gauntlet@gmail.com>
2019-07-19 12:36:56 -0400

    Update aws_s3.py

--- aws_s3.py
+++ aws_s3.py
@@ -1,4 +1,7 @@
 
+import boto3
+import logging
+
 # Upload File to S3 in CSV format from list to CSV
 def uploadS3(value=[],timestamp=[],bucketname="",path=""):
     """
@@ -6,21 +9,22 @@ def uploadS3(value=[],timestamp=[],bucketname="",path=""):
     timestamp : list
     Both value, timestamp are of equal length
     """
-    try:
-    
+    try:    
+      # Ex list values, substitute with own
       value=["234","345","345","445","346"]
       timestamp= ["tue","wed","thu","fri","sat"]
 
+      # Add comma seperator and new line for list value
       csv_file=""
       for i,j in zip(value,timestamp):
          csv_file+="i"+","+j
          csv_file+="\n"
-
-      # Method 1: Object.put()
+            
+      # Upload using boto3 put
       s3 = boto3.resource('s3')
       obj = s3.Object(bucketname, path)
       obj.put(Body=csv_file)
       return 1
     except Exception as e:
         logging.error(e)
-        return 
+        return 0
